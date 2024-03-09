3a8dd4ded72b4cdf321379b1bdce9f9dbcf165a2
Darcy Shen <da@liii.pro>
2024-03-09 20:00:57 +0800

    wip

--- Kernel/Types/tree.hpp
+++ Kernel/Types/tree.hpp
@@ -8,3 +8,27 @@
 #pragma once
 
 #include "observer.hpp"
+
+using lolly::data::A;
+using lolly::data::is_atomic;
+
+inline int
+hash (tree t) {
+  if (is_atomic<observer> (t)) return hash (t->label);
+  else return t->op ^ hash (A<observer> (t));
+}
+
+inline int
+hash (array<tree> a) {
+  int i, h= 0, n= N (a);
+  for (i= 0; i < n; i++) {
+    h= (h << 7) + (h >> 25);
+    h= h + hash (a[i]);
+  }
+  return h;
+}
+
+inline int
+right_index (tree t) {
+  return is_atomic (t) ? N (t->label) : 1;
+}
